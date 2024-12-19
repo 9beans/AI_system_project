f79aadee89070a52fced346d59d8eb3e8695913c
9beans <9sempiternal@naver.com>
2024-12-20 03:38:02 +0900

    [Final] ...

--- plot.py
+++ plot.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 from project import target_model
 
 def scale_systolic_dim_latency_graph(scale_systolic_dim_latency_results_, subplot_col_):
+    main_fontsize = 15
+    small_fontsize = 12
     # Number of subplots (rows and columns) based on the number of scales
     num_scales = len(scale_systolic_dim_latency_results_)
     num_cols = subplot_col_
     num_rows = (num_scales + num_cols - 1) // num_cols  # Calculate required rows for the number of subplots
 
     # Create subplots
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 10))
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(13, 10))
     axes = axes.flatten()
 
     # Plot for each scale
@@ -28,12 +30,14 @@ def scale_systolic_dim_latency_graph(scale_systolic_dim_latency_results_, subplo
         core = scale % 8
         if core == 0:
             core = 8
-        ax.set_title(f"Scale (card, core): ({card}, {core})")
-        ax.set_xlabel("Systolic Dimension")
-        ax.set_ylabel("Latency (sec)")
+        ax.set_title(f"Scale (card, core): ({card}, {core})", fontsize = small_fontsize)
+        ax.set_xlabel("Systolic Dimension", fontsize = small_fontsize)
+        if (idx == 0) | (idx == 4):
+            ax.set_ylabel("Latency (sec)", fontsize = small_fontsize)
         ax.legend()
         ax.grid(True, linestyle='--', alpha=0.6)
-        ax.tick_params(axis='x', rotation=45)
+        ax.tick_params(axis='x', rotation=45, labelsize = small_fontsize)
+        ax.tick_params(axis='y', labelsize = small_fontsize)
 
     # Remove any unused subplots
     for idx in range(len(scale_systolic_dim_latency_results_), len(axes)):
@@ -41,7 +45,7 @@ def scale_systolic_dim_latency_graph(scale_systolic_dim_latency_results_, subplo
 
     # Adjust layout
     plt.tight_layout()
-    plt.savefig(f"{target_model[3]}_Explore_Core_Config_with_System_Scaling.png", format="png", dpi=300)
+    plt.savefig(f"{target_model[3]}_Explore_Core_Config_with_System_Scaling.png", format="png", dpi=300, fontsize = small_fontsize)
 
 def scale_systolic_dim_throughput_graph(scale_systolic_dim_throughput_results_):
     import matplotlib.cm as cm
